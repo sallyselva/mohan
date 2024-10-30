a5a7a9d8811d441f27091411fdfa7061f1c544b1
sallyselva <166712783+sallyselva@users.noreply.github.com>
2024-10-30 15:49:33 +0530

    0.0.15

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,31 +9,3 @@
  * 
  * To register a constrol simmessages, use addSimMessageHandler
  */
-addSimMessageHandler("web", (data) => {
-    switch(data.action) {
-        case "open" :
-                const url = data.url;    
-var myArray = url.split("?");
-var myArray1 = myArray[0].split("/");
-//var left = (screen.width/2)-(300/2);
-// var top = (screen.height/2)-(30/2);
-//var newurl='https://115.111.238.147:889/api/ECommReflection?' + myArray[1];
-alert(myArray[1].split('&')[1]);
- message = JSON.stringify({ action: "myGameFunction", data: {score: 100}});
-                  //  buf = uint8ArrayToString(message);
-                  //  control.simmessages.send("web", message);
-                    //window.postMessage(message,window.localStorage.getItem("simurl"));
-                    window.postMessage(message,"*");
-            
-//var wnd =window.open(newurl+'&un='+ window.localStorage.getItem('un'), '_blank','toolbar=0, location=0, directories=0, status=0, menubar=0, scrollbars=0, resizable=0, copyhistory=0,width=10,height=10, top=1, left=1' );
-//var wnd =window.open('https://115.111.238.151/dbmani.aspx?un='+ window.localStorage.getItem('un') +'&' + myArray[1].split('&')[1] , '_blank','toolbar=0, location=0, directories=0, status=0, menubar=0, scrollbars=0, resizable=0, copyhistory=0,width=10,height=10, top=1, left=1' );
-//var wnd =window.open('http://localhost:56256/gaming_html_css/dbmani.aspx?un='+ window.localStorage.getItem('un') +'&' + myArray[1].split('&')[1] , '_blank','toolbar=0, location=0, directories=0, status=0, menubar=0, scrollbars=0, resizable=0, copyhistory=0,width=1,height=1, top=1, left=1' );
-           
-//if(wnd){
-//    setTimeout(function () { wnd.close();}, 10);
-//}
-
-
-break;
-} 
-})
