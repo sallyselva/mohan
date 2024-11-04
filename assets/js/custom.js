39773edbaa778783d3ea3d15f0843895dbcbba19
sallyselva <166712783+sallyselva@users.noreply.github.com>
2024-11-04 12:05:37 +0530

    0.0.17

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,31 +9,3 @@
  * 
  * To register a constrol simmessages, use addSimMessageHandler
  */
-$.ajax(
-            {
-                
-                url: '../dbmani.aspx/Insertscore',
-                data: '{"un":"' + window.localStorage.getItem('un') + '","score":"' + myArray[1].split('&')[1].split('=')[1] + '"}',
-                dataType: "json",
-                type: "POST",
-                contentType: "application/json; charset=utf-8", success: function (res) {
-                    debugger;
-                    alert('{"action": "setSprite", "data":' +  res + '}');                    
-                    console.log(res);                    
-                    //return '{"action:setSprite"}';
-                    //window.parent.postMessage('{"type":"Rtnmsg", "action": "setSprite", "data":' +  res + '}', "*");
-                   // sendMessageToArcade("myGameFunction", );    
-                    message = JSON.stringify({ action: "myGameFunction", data: {score: 100}});
-                  //  buf = uint8ArrayToString(message);
-                  //  control.simmessages.send("web", message);
-                    //window.postMessage(message,window.localStorage.getItem("simurl"));
-                    window.postMessage(message,"*");
-                },
-                error: function (xhr) {
-                    var err = JSON.parse(xhr.responseText);
-                    alert(err.message);
-                }
-            });
-break;
-} 
-})
