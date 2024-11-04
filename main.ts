73ec223b72437f4b1da4308c2303aa03479e70c3
sallyselva <166712783+sallyselva@users.noreply.github.com>
2024-11-04 16:49:17 +0530

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -206,3 +206,36 @@ game.onUpdateInterval(5000, function () {
         `, randint(-50, 50), randint(-50, 50))
     bee.setKind(SpriteKind.Enemy)
 })
+
+namespace web {
+    const CHANNEL = "web";
+
+    function sendJSON(json: any) {
+        const msg = JSON.stringify(json);
+        const buf = Buffer.fromUTF8(msg);
+        control.simmessages.send(CHANNEL, buf);
+    }
+
+    export function open(url: string) {
+        sendJSON({ action: "open", url: url });
+    }
+
+    export function sendScore(score: number) {
+        sendJSON({ action: "sendScore", score: score });
+    }
+
+    control.simmessages.onReceived(CHANNEL, (buf: Buffer) => {
+        console.log("Message received in MakeCode:"+ buf.toString());
+        const msg = JSON.parse(buf.toString());
+        if (msg.action === "sendScore") {
+            handleReceivedScore(msg.score);
+        } else if (msg.action === "open") {
+            open(msg.url);
+        }
+    });
+
+    function handleReceivedScore(score: number) {
+        info.setScore(score);
+        console.log("Score received: " + score);
+    }
+}
