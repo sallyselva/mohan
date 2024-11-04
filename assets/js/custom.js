bfc5529a91a14a8a70a9b3d8870abaed30df9c77
sallyselva <166712783+sallyselva@users.noreply.github.com>
2024-11-04 18:44:07 +0530

    0.0.24

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,33 +9,3 @@
  * 
  * To register a constrol simmessages, use addSimMessageHandler
  */
-addSimMessageHandler("web", (data) => {
-    console.log("Message received in custom.js:", data);
-    switch (data.action) {
-        case "open":
-            const url = data.url;
-            console.log("Opening URL:", url);
-            const myArray = url.split("?");
-            const score = myArray[1].split('&')[1].split('=')[1];
-
-            $.ajax({
-                url: '../dbmani.aspx/Insertscore',
-                data: JSON.stringify({ un: window.localStorage.getItem('un'), score: score }),
-                dataType: "json",
-                type: "POST",
-                contentType: "application/json; charset=utf-8",
-                success: function (res) {
-                    console.log('Score inserted:', res);
-                    const message = JSON.stringify({ action: "myGameFunction", data: { score: score } });
-                    console.log("Posting message:", message);
-                    window.postMessage(message, "*");
-                },
-                error: function (xhr) {
-                    const err = JSON.parse(xhr.responseText);
-                    console.error(err.message);
-                    alert(err.message);
-                }
-            });
-            break;
-    }
-});
